c7ef1d5f157b72aab5c957213daa7894004317fd
TungHoyu <dhy342698@icloud.com>
2024-05-17 18:30:52 +0800

    集成阿里OSS存储文件

---
+++ src/main/java/com/albedo/bigevent/utils/AliOssUtil.java
@@ -0,0 +1,59 @@
+package com.albedo.bigevent.utils;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class AliOssUtil {
+
+    //bucket节点
+    private static final String ENDPOINT = null;
+    //key
+    private static final String ACCESS_KEY_ID = null;
+    //secret
+    private static final String ACCESS_KEY_SECRET = null;
+    //bucketName
+    private static final String BUCKET_NAME = null;
+
+
+    public static String uploadFile(String objectName, InputStream in) throws Exception {
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(ENDPOINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
+
+        //url  https://bucketName.endpoint/objectName
+        String url = "";
+        try {
+
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(BUCKET_NAME, objectName, in);
+
+            // 上传字符串。
+            PutObjectResult result = ossClient.putObject(putObjectRequest);
+            url = "https://"+BUCKET_NAME+"."+ENDPOINT.substring(ENDPOINT.lastIndexOf("/")+1)+"/"+objectName;
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+        return url;
+    }
+}
