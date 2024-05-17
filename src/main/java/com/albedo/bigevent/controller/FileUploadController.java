1dc560a3fa8cde0d1a9ce310f38a76d575a0b80a
TungHoyu <dhy342698@icloud.com>
2024-05-17 19:24:11 +0800

    项目部署

--- src/main/java/com/albedo/bigevent/controller/FileUploadController.java
+++ src/main/java/com/albedo/bigevent/controller/FileUploadController.java
@@ -2,13 +2,11 @@ package com.albedo.bigevent.controller;
 
 import com.albedo.bigevent.pojo.Result;
 import com.albedo.bigevent.utils.AliOssUtil;
-import com.sun.org.apache.xml.internal.security.utils.resolver.implementations.ResolverXPointer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+
 import java.util.UUID;
 
 @RestController
