cdcac37c944de89f699e7a2e6c1556aea174e692
TungHoyu <dhy342698@icloud.com>
2024-05-28 19:14:20 +0800

    解决了前端发送字符串类型的时间请求时，提示参数转换错误的问题

--- src/main/java/com/albedo/bigevent/pojo/User.java
+++ src/main/java/com/albedo/bigevent/pojo/User.java
@@ -27,8 +27,8 @@ public class User {
     @Email
     private String email;//邮箱
     private String userPic;//用户头像地址
-    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;//创建时间
-    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;//更新时间
 }
